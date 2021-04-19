cacfb4098821221a27ec29a742dbe2224d3d2575
leejina90 <leejinaweb0@gmail.com>
2021-04-19 17:22:23 +0900

    4월 19일 수업내용

--- easys_publishing/js/common.js
+++ easys_publishing/js/common.js
@@ -5,4 +5,45 @@
 		tablet:801,
 		mobile:800
 	};
+
+	function scrollShowHide(status) {
+		$html.css({"overflow-y":status});
+		return $html.width();
+	}
+
+	var sc_w1 = scrollShowHide("hidden"), // 스크롤이 없을때 width
+	sc_w2 = scrollShowHide("scroll"), // 스크롤이 있을때 width
+	sc_w3 = sc_w1 - sc_w2;
+
+	if(sc_w3 > 0) {
+		deviceSize.pc = deviceSize.pc - sc_w3;
+		deviceSize.tablet = deviceSize.tablet - sc_w3;
+		deviceSize.mobile = deviceSize.mobile - sc_w3;
+	}
+
+	console.log(deviceSize.pc);
+
+	$(win).on("resize", function(){
+		var w_size = $(win).width();
+
+		if(w_size >= deviceSize.pc && !$("html").hasClass("pc")) {
+			$html.removeClass("mobile tablet").addClass("pc");
+			scrollShowHide("scroll");
+		} 
+
+		else if(w_size < deviceSize.pc && w_size >= deviceSize.tablet && !$("html").hasClass("tablet")) {
+			$html.removeClass("mobile pc").addClass("tablet");
+			scrollShowHide("scroll");
+		} 
+
+		else if(w_size < deviceSize.mobile && !$("html").hasClass("mobile")) {
+			$html.removeClass("pc tablet").addClass("mobile");
+			var menu_pos = parseInt($(".mobile-menu-wrap").css("left"));
+
+			if(menu_pos >= 0) {
+				scrollShowHide("hidden");
+			}
+		}
+	});
+
 }(window, jQuery));
